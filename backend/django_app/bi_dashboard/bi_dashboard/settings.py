f29e8a9045164419895887e7f702390ac26f3e62
kdkaanev <kdkaane@github.io>
2026-01-16 18:22:45 +0200

    up docker compose

--- backend/django_app/bi_dashboard/bi_dashboard/settings.py
+++ backend/django_app/bi_dashboard/bi_dashboard/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zv*uhywb*425s$#kp35rf#v=749s9&j3(qfy7cnso0aaf&x9l_'
+SECRET_KEY = ')$hi%0pis8z7syn@(ulb(%)34y7$94jytmb2try##l02am%&)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
