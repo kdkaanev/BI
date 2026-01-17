51f81d247353aa1da7c0320ac7f101e39838a985
kdkaanev <kdkaane@github.io>
2026-01-17 18:20:57 +0200

    up

--- backend/django_app/bi_dashboard/bi_dashboard/settings.py
+++ backend/django_app/bi_dashboard/bi_dashboard/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
+DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
