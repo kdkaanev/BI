bedcdaea21ce650bbcfa1b2cfdd622a4885e4e12
kdkaanev <kdkaane@github.io>
2026-02-07 09:54:44 +0200

    DEBUG set to false

--- backend/django_app/bi_dashboard/bi_dashboard/settings.py
+++ backend/django_app/bi_dashboard/bi_dashboard/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-default-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.getenv("DEBUG", "True") == "True"
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "True") == "True"
+#DEBUG = True
 
 ALLOWED_HOSTS = ["api.mybi.store",]
 
