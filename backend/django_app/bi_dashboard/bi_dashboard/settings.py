6c16419e778ebd9bf579476710c454e99f328c1d
kdkaanev <kdkaane@github.io>
2026-02-22 15:50:26 +0200

    up celery setting

--- backend/django_app/bi_dashboard/bi_dashboard/settings.py
+++ backend/django_app/bi_dashboard/bi_dashboard/settings.py
@@ -224,9 +224,13 @@ else:
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+if DEBUG:
+     CELERY_BROKER_URL = "redis://localhost:6379/0"
+     CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+else:
+     CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+     CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
 
 CELERY_ACCEPT_CONTENT = ["json"]
 
